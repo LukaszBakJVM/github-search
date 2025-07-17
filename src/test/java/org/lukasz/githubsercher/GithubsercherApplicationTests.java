0db46a6318ce4ebfd21e749e5aca77ffee398a12
Łukasz Bąk <lukasz.bak@interiowy.pl>
2025-07-17 12:57:06 +0200

    test

--- src/test/java/org/lukasz/githubsercher/GithubsercherApplicationTests.java
+++ src/test/java/org/lukasz/githubsercher/GithubsercherApplicationTests.java
@@ -1,8 +1,13 @@
 package org.lukasz.githubsercher;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.lukasz.githubsercher.controller.GithubController;
+import org.lukasz.githubsercher.dto.RepositoryDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -11,7 +16,10 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.List;
+
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GithubsercherApplicationTests {
@@ -22,6 +30,10 @@ class GithubsercherApplicationTests {
     static WireMockExtension wireMockServer = WireMockExtension.newInstance().options(wireMockConfig().port(dynamicPort)).build();
     @Autowired
     WebTestClient webTestClient;
+    @Autowired
+    ObjectMapper objectMapper;
+    @Autowired
+    private GithubController githubController;
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
@@ -36,6 +48,19 @@ class GithubsercherApplicationTests {
 
 
         webTestClient.get().uri("/repositories/" + username).accept(MediaType.APPLICATION_JSON).exchange().expectStatus().isOk().expectBody().json(Response.jsonData);
+
+    }
+
+    @Test
+    void testGetRepositoriesWithWireMock() throws JsonProcessingException {
+
+
+        List<RepositoryDto> repos = githubController.getGithubRepositories("octocat");
+
+        List<RepositoryDto> expected = objectMapper.readValue(Response.jsonData, new TypeReference<>() {
+        });
+        assertThat(repos).isEqualTo(expected);
+
 
     }
 
